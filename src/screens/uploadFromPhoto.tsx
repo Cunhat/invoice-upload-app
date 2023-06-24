c30514ade328294742c25b2d9d145d8f5055e092
Tiago Cunha <tiago92cunha@gmail.com>
2023-06-24 15:32:10 +0100

    Add preview photo screen and save photo to gallery

--- src/screens/uploadFromPhoto.tsx
+++ src/screens/uploadFromPhoto.tsx
@@ -7,17 +7,26 @@ import {
   View,
   Image,
   Button,
+  TouchableOpacity,
 } from "react-native";
-import { Camera, CameraType } from "expo-camera";
+import { Camera, CameraCapturedPicture, CameraType } from "expo-camera";
 import * as MediaLibrary from "expo-media-library";
 import { MainLayout } from "../components/layouts/mainLayout";
+import { RefreshCcw, Home } from "lucide-react-native";
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
+import type { RootStackParamList } from "../../App";
 
-export const UploadFromPhoto = () => {
-  let cameraRef = useRef(null);
-  const [photo, setPhoto] = React.useState<string>("");
-
+export const UploadFromPhoto: React.FC<
+  NativeStackScreenProps<RootStackParamList, "UploadFromPhoto">
+> = ({ navigation }) => {
+  // let cameraRef = useRef(null);
+  const [photo, setPhoto] = React.useState<CameraCapturedPicture | undefined>(
+    undefined
+  );
+  const [camera, setCamera] = useState<Camera | null>(null);
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [disableButton, setDisableButton] = useState(false);
 
   useEffect(() => {
     console.log("permission", typeof permission);
@@ -27,16 +36,15 @@ export const UploadFromPhoto = () => {
   }, []);
 
   const takePic = async () => {
-    console.log("takePic");
     let options = {
       quality: 1,
       base64: true,
       exif: false,
     };
 
-    let newPhoto = await cameraRef?.current?.takePictureAsync(options);
+    const data = await camera?.takePictureAsync(options);
 
-    setPhoto(newPhoto);
+    setPhoto(data);
   };
 
   if (photo) {
@@ -46,23 +54,49 @@ export const UploadFromPhoto = () => {
     //   });
     // };
 
-    // let savePhoto = () => {
-    //   MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
-    //     setPhoto(undefined);
-    //   });
-    // };
+    const savePhoto = () => {
+      MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
+        setPhoto(undefined);
+      });
+    };
+
+    const uploadPhoto = () => {
+      savePhoto();
+      navigation.navigate("Home");
+    };
 
     return (
-      <SafeAreaView className="flex-1 bg-black">
+      <SafeAreaView className="flex-1 bg-black relative">
         <Image
           source={{ uri: "data:image/jpg;base64," + photo.base64 }}
           className="items-stretch flex-1"
         />
-        {/* <Button title="Share" onPress={sharePic} />
-        {hasMediaLibraryPermission ? (
-          <Button title="Save" onPress={savePhoto} />
-        ) : undefined} */}
-        <Button title="Discard" onPress={() => setPhoto("")} />
+        <View className="bg-black/60 h-[200px] z-10 justify-center items-center  mt-auto absolute w-full bottom-0 flex flex-row">
+          <Pressable
+            key={"goHomeButton"}
+            className="bg-white/20 w-[15%] h-[30%] rounded-full flex justify-center items-center"
+            onPress={() => navigation.navigate("Home")}
+          >
+            <Home className="text-white" width={"90%"} height={"50%"} />
+          </Pressable>
+          <Pressable
+            key={"uploadPhoto"}
+            className="bg-white w-[20%] h-[40%] rounded-full flex justify-center items-center shadow-lg mx-10"
+            onPress={() => uploadPhoto()}
+          >
+            <RefreshCcw className="text-white" width={"90%"} />
+          </Pressable>
+          <Pressable
+            key={photo.uri}
+            className="bg-white/20 w-[15%] h-[30%] rounded-full flex justify-center items-center"
+            onPress={() => {
+              setPhoto(undefined);
+              setDisableButton(!disableButton);
+            }}
+          >
+            <RefreshCcw className="text-white" width={"90%"} height={"50%"} />
+          </Pressable>
+        </View>
       </SafeAreaView>
     );
   }
@@ -85,9 +119,15 @@ export const UploadFromPhoto = () => {
   }
 
   return (
-    <Camera className="flex-1" ref={cameraRef}>
+    <Camera className="flex-1" ref={(ref) => setCamera(ref)}>
       <View className="bg-black/60 h-[200px] z-10 justify-center items-center mt-auto">
-        <CameraButton onPress={() => takePic()} />
+        <CameraButton
+          onPress={() => {
+            setDisableButton(!disableButton);
+            takePic();
+          }}
+          disabled={disableButton}
+        />
       </View>
     </Camera>
   );
